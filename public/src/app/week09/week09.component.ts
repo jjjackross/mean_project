07c60a666e225c2de448c51761388c10288c0809
[John Urbanek] <[Johnurbanek86@gmail.com]>
2019-10-25 11:03:08 -0500

    first commit

--- public/src/app/week09/week09.component.ts
+++ public/src/app/week09/week09.component.ts
@@ -8,8 +8,59 @@ import { Component, OnInit } from '@angular/core';
 export class Week09Component implements OnInit {
 
   constructor() { }
-
+  prayer = 0
+  
   ngOnInit() {
   }
+  e1_correctAnswer() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome2").style.display = 'inline-block';
+  }
+
+  e1_incorrectAnswer() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome3").style.display = 'inline-block';
+    this.prayer ++
+    console.log(this.prayer)
+  }
+  e1_incorrectAnswer2() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome1").style.display = 'inline-block';
+  }
+
+  nextEvent() {
+    document.getElementById("e1_container1").style.display = 'none';
+    document.getElementById("e2_container2").style.display = 'inline-block';
+  }
+
+  e2_correctAnswer() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome2").style.display = 'inline-block';
+    this.prayer = 0
+  }
+
+  e2_incorrectAnswer() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    if(this.prayer > 0){
+      document.getElementById("e2_outcome4").style.display = 'inline-block';
+      this.prayer = 0
+    }
+    else{
+    document.getElementById("e2_outcome1").style.display = 'inline-block';
+    this.prayer = 0
+  }
+  }
+  e2_incorrectAnswer2() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome3").style.display = 'inline-block';
+    this.prayer = 0
+  }
+
 
 }
--- public/src/app/week12/week12.component.ts
+++ public/src/app/week12/week12.component.ts
@@ -11,5 +11,48 @@ export class Week12Component implements OnInit {
 
   ngOnInit() {
   }
+  e1_correctAnswer() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome2").style.display = 'inline-block';
+    
+  }
 
+  e1_incorrectAnswer() {
+    
+      document.getElementById("e1_prompt").style.display = 'none';
+      document.getElementById("e1_buttons").style.display = 'none';
+      document.getElementById("e1_outcome3").style.display = 'inline-block'; 
+  }
+  e1_incorrectAnswer2() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome1").style.display = 'inline-block';
+    
+  }
+
+  nextEvent() {
+    document.getElementById("e1_container1").style.display = 'none';
+    document.getElementById("e2_container2").style.display = 'inline-block';
+  }
+
+  e2_correctAnswer() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome2").style.display = 'inline-block';
+    
+  }
+
+  e2_incorrectAnswer() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome1").style.display = 'inline-block'; 
+  }
+  
+  e2_incorrectAnswer2() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome3").style.display = 'inline-block';
+    
+  }
 }
