dc13836a2daab884b5753d249201dad6c3f85d34
jjjackross <jackrossfilm@outlook.com>
2019-10-24 23:32:08 -0500

    Night commit

--- public/src/app/week07/week07.component.ts
+++ public/src/app/week07/week07.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-week07',
@@ -12,4 +13,38 @@ export class Week07Component implements OnInit {
   ngOnInit() {
   }
 
+  e1_correctAnswer() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome1").style.display = 'inline-block';
+  }
+
+  e1_incorrectAnswer() {
+    document.getElementById("e1_prompt").style.display = 'none';
+    document.getElementById("e1_buttons").style.display = 'none';
+    document.getElementById("e1_outcome2").style.display = 'inline-block';
+  }
+
+  nextEvent() {
+    document.getElementById("e1_container1").style.display = 'none';
+    document.getElementById("e2_container2").style.display = 'inline-block';
+  }
+
+  e2_incorrectAnswer1() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome1").style.display = 'inline-block';
+  }
+
+  e2_incorrectAnswer2() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome2").style.display = 'inline-block';
+  }
+
+  e2_correctAnswer() {
+    document.getElementById("e2_prompt").style.display = 'none';
+    document.getElementById("e2_buttons").style.display = 'none';
+    document.getElementById("e2_outcome3").style.display = 'inline-block';
+  }
 }
